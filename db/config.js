63be297f05d01d7cf58c724f0982e5702c673008
shinsangeun <s0813se@naver.com>
2021-03-22 18:45:07 +0900

    clearDB config 추가

--- db/config.js
+++ db/config.js
@@ -1,10 +1,23 @@
 const mysql = require('mysql');
+
+// local config
+/*
 const db_info = {
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
     password: 'ssee',
     database: 'nailOllae'
+}
+*/
+
+// heroku config
+const db_info = {
+    host: 'us-cdbr-east-03.cleardb.com',
+    port: '3306',
+    user: 'bc06ae6dbc0345',
+    password: '00cba21a',
+    database: 'heroku_4c235e2ad6c8c27'
 }
 
 module.exports = {
